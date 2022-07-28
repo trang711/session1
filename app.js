a78e1c218c55adf834375c388f1658a68dc07c6b
Mai Huyền Trang <71455748+trang711@users.noreply.github.com>
2022-07-29 01:31:22 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+//Ex1: Viết chương trình nhập điểm của một sinh viên cho các môn: 
+//Vật lý, Hóa học, và Sinh học. 
+//Sau đó hiển thị điểm trung bình và tổng của những điểm này.
+
+//Giải:
+
+//let a = prompt("Nhập điểm môn Vật Lý");
+//let b = prompt("Nhập điểm môn Hóa Học");
+//let c = prompt("Nhập điểm môn Sinh Học");
+
+//let TB = (Number(a) + Number(b) + Number(c))/3;
+//let Tong = Number(a) + Number(b) + Number(c);
+
+//alert(` tổng 3 môn là ${Tong} và trung bình 3 môn là ${TB}`);
+
+//Ex2: Viết chương trình nhập một giá trị là độ 0C (Celsius) và chuyển nó sang độ 0F (Fahrenheit). 
+//[Hướng dẫn: C/5 = (F-32)/9]
+
+//Giải:
+
+//let C = prompt("Nhập nhiệt độ C vào:");
+//let sub = C * 1.8 + 32;
+//alert(`Nhiệt độ F sau khi quy đổi là ${sub} độ F`);
+
+//Ex3: Viết chương trình tính diện tích hình tròn
+
+//Giải:
+
+//let t = prompt("Nhập bán kính hình tròn");
+//let sqrt = t*t*Math.PI;
+//alert(`Diện tích hình tròn là ${sqrt}`);
+
+//Ex4: Viết chương trình chu vi hình tròn
+
+//Giải:
+
+//let h = prompt("Nhập bán kính hình tròn");
+//let sqrt = 2*h*Math.PI;
+//alert(`Chu vi hình tròn là ${sqrt}`);
